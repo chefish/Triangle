915a327677ac3b3a028b54b18d97bd959f120b5e
剑六 <xiaomin.yxm@alibaba-inc.com>
2018-01-05 17:25:04 +0800

    add y

--- app/src/main/java/com/fish/triangle/MainActivity.java
+++ app/src/main/java/com/fish/triangle/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     TriangleView leftView;
-
+    int y;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
